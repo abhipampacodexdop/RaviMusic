f5ec954eb13caad4c1d13385da6996665828792e
abhipampacodexdop <140977883+abhipampacodexdop@users.noreply.github.com>
2023-11-01 18:36:18 +0530

    Update config.py
    Kk

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Pranav-Saraswat/Testing-Alpha1",
+    "https://github.com/abhipampacodexdop/RaviMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
